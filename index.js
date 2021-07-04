e3f57e3b2eeb7f1bb1e9d49fe7a58d0d4b1d2663
Luke Otwell <lukehenryotwell@gmail.com>
2021-07-04 13:24:02 -0400

    commit tree bc6d0d9fe68db4e98a0011ed8185fe54c98c217e

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+// document.addEventListener("DOMContentLoaded", function () {
+//     console.log("The DOM HAS Loaded")
+// })
+
+// console.log("Fire Away");
+textFunction = () => {
+    document.getElementById("text").textContent = "This is really cool!"
+}
+
+document.addEventListener("DOMContentLoaded", textFunction)
